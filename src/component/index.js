f98f91996e165a62c205b025b0fdacbfdbb1280c
Dipak <deepraj41352@gmail.com>
2023-06-20 19:00:31 +0530

    added useMemoHooks

--- src/component/index.js
+++ src/component/index.js
@@ -1,31 +1,11 @@
-// usecallback
-import { useCallback } from 'react';
-import React, {useState} from 'react'
-import Child from '../component1';
+// useMemo
+import React from 'react'
 
 export default function UseCallbackHooks() {
-  const[countOne  , setCountOne] = useState(0)
-  const[countTow, setCountTwo] = useState(0)
-
-  function increment(){
-    setCountOne(countOne+1)
-  }
-  function decrement(){
-    setCountTwo(countTow-1)
-  }
-  const getItem=useCallback(()=>{
-    console.log(countTow+1,countTow-1);
-for(let i=0;i<1000000000;i++){}
-console.log('gertitrm')
-    return [countTow+1,countTow-1];
-},[countTow])
 
   return (
     <div>
-      <button onClick={increment}>click</button>
-      <button onClick={decrement}>click</button>
-      <p>{countOne} || {countTow}</p>
-      <Child getItem={getItem}/>
+     
     </div>
   )
 }
