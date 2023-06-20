082c9a1f30a7acee12c7feb69c32bac1c9e813f9
Dipak <deepraj41352@gmail.com>
2023-06-20 14:13:08 +0530

    complete useEffect

--- src/component/index.js
+++ src/component/index.js
@@ -1,10 +1,21 @@
-// useState
-import React from 'react'
+// useEffect
+import React, { useEffect, useState } from 'react'
 
-export default function index() {
+export default function UseEffectHooks() {
+  
+  const[count, setCount] = useState(0)
+  const[calculation, setCalculation] = useState(0)
+  function handalClick(){
+    setCount(count+1)
+  }
+  useEffect(()=>{
+    setCalculation(count*2)
+  },[count])
   return (
     <div>
-      
+      <p>{count}</p>
+      <button onClick={handalClick}>Click</button>
+      <p>{calculation}</p>
     </div>
   )
 }
