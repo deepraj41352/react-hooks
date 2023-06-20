ab1a17804ef258e844d8c37fbf372723aac4eb7e
Dipak <deepraj41352@gmail.com>
2023-06-20 16:42:22 +0530

    added useCallBack

--- src/component/index.js
+++ src/component/index.js
@@ -1,20 +1,11 @@
-// useContext
+// useRef
 import React from 'react'
-import { useRef } from "react";
 
 export default function UseContextHooks() {
 
-
-  const inputElement = useRef();
-
-  const focusInput = () => {
-    inputElement.current.focus();
-  };
-
   return (
     <div>
-      <input type="text" ref={inputElement} />
-      <button onClick={focusInput}>Focus Input</button>
+     
     </div>
   )
 }
