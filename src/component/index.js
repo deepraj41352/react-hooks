610966189a07260efc2e00b9c3dbe4308204eb8b
Dipak <deepraj41352@gmail.com>
2023-06-20 19:08:29 +0530

    complete useMemo

--- src/component/index.js
+++ src/component/index.js
@@ -1,11 +1,29 @@
 // useMemo
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 
 export default function UseCallbackHooks() {
+  const [countOne, setCountOne] = useState(0)
+    const [countTow, setCountTwo] = useState(0)
 
-  return (
+    function increment(){
+        setCountOne(countOne+1);
+    }
+
+    function decrement(){
+        setCountTwo(countTow-1);
+    }
+
+    const isEeven=useMemo(()=>{
+
+        for(let i=0;i<1000000000;i++){}
+        return countOne%2===0;
+    },[countOne])
+   
+
+    return (
     <div>
-     
+      <button onClick={increment}>count One {countOne}</button> {isEeven?'even':'odd'}<br></br>
+      <button onClick={decrement}>count Two {countTow}</button>
     </div>
   )
 }
